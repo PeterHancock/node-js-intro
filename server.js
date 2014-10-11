8e501bc652bcddc3c78963cd353b561b71536ba7
Peter Hancock <peter.hancock@gmail.com>
2014-10-11 11:07:45 +0100

    Make terminal shell more configurable

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var argv = require('minimist')(process.argv.slice(2));
 
 var port = argv.p || 8080;
 
-var shellCmd = argv.docker ? 'docker run -it --rm ' + argv.docker + ' /bin/bash' : '/bin/bash -i';
+var shellCmd = argv.shell ? argv.shell : '/bin/bash -i';
 
 var server = http.createServer(ecstatic(__dirname));
 
